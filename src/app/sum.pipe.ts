1156e5c6fb385178a56498560b7caf2296f38a17
Matthias Muehlich <mmuehlich@gmx.li>
2017-12-16 00:02:24 +0100

    fix guest list - graphics added

--- src/app/sum.pipe.ts
+++ src/app/sum.pipe.ts
@@ -3,8 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'sum' })
 export class SumPipe implements PipeTransform {
     transform(items: any[], attr: string): any {
-        if (items)
-            return items.reduce((a,b) => b[attr] == undefined ? a : a + Number(b[attr]), 0);
-        return 0;
+        if (!items)
+            return 0;
+        
+        return items.reduce((a,b) => b[attr] == undefined ? a : a + b[attr].length, 0);
     }
 }
